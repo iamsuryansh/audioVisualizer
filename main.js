18a27b893a72050fa49b7b33039f87e084502945
iamsuryansh <suryanshjain531@gmail.com>
2022-08-27 16:23:02 +0530

    added full github.com path for audio source

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ window.addEventListener('load', function(){
     selectAudioBtn.addEventListener('click', function() {
 
     const audio1 = document.getElementById("audio1");
-      audio1.src = './audioSamples/' + document.getElementById("mySelect").value + '.mp3';
+      audio1.src = 'https://github.com/iamsuryansh/audioVisualizer/blob/master/audioSamples/' + document.getElementById("mySelect").value + '.mp3';
       audio1.play();
       const audioContext = new AudioContext();
       console.log(audioSource)
